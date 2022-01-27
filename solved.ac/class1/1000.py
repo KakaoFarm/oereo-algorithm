5e994860cfe1cbeb480d1d3b2150592d91bc22b0
oereo <dlstpgns0406@gmail.com>
2022-01-27 23:37:57 +0900

    chore: base setting

---
+++ solved.ac/class1/1000.py
@@ -0,0 +1,14 @@
+# A+B bronze5
+
+def main():
+    first_number, second_number = get_numbers()
+    return first_number + second_number
+
+
+def get_numbers():
+    first_number, second_number = map(int, input().split())
+    return first_number, second_number
+
+
+print(main())
+
