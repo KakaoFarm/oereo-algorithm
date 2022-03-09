90cbabe4ced9515db24cbc3bd7bed086eebfd5a3
oereo <dlstpgns0406@gmail.com>
2022-03-09 18:27:05 +0900

    feat: 1로 만들기

---
+++ solved.ac/class3/1463-1로_만들기.py
@@ -0,0 +1,13 @@
+dp = [0 for i in range(1000001)]
+
+
+N = int(input())
+
+for i in range(2, N+1):
+    dp[i] = dp[i - 1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i // 3] + 1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i // 2] + 1, dp[i])
+
+print(dp[N])
